49763d93a7b96b178cce208a9cb7b37d7c84013f
marcelldac <dacmarcell@gmail.com>
2024-05-12 22:45:54 -0300

    feat: add data file

---
+++ data.js
@@ -0,0 +1,35 @@
+export const data = [
+    {
+      title: 'Camarão ao molho',
+      description: 'Prepare-se para uma explosão de sabores! Nosso Camarão ao Molho é uma experiência gastronômica inigualável. Delicie-se com a suculência do camarão combinada com a riqueza e o aroma do nosso molho exclusivo. Cada garfada é uma jornada sensorial que o transportará diretamente para as águas profundas do mar. Venha saborear esta obra-prima em nosso restaurante e deixe-se levar pela harmonia dos sabores. Satisfação garantida em cada mordida. Mergulhe nessa experiência única!',
+      url: 'https://assets.unileversolutions.com/recipes-v2/215163.jpg'
+    },
+    {
+      title: 'Nosso delicioso camarão empanado',
+      description: 'Em cada mordida, uma explosão de sabor! Nosso delicioso Camarão Empanado é uma verdadeira tentação para os amantes de frutos do mar. Crocante por fora e suculento por dentro, cada camarão é cuidadosamente preparado para garantir uma experiência gastronômica inesquecível. Na Casa do Camarão, elevamos o camarão empanado a um novo patamar de perfeição, utilizando ingredientes frescos e técnicas artesanais. Venha experimentar essa delícia em nosso restaurante e descubra por que nosso camarão empanado é tão aclamado. Uma vez que você prove, será impossível resistir. Aguardamos sua visita para uma verdadeira festa de sabores na Casa do Camarão!z',
+      url: 'https://s2.glbimg.com/tG3TBBfeETzX2ckBWisgM_GmXCQ=/620x480/smart/e.glbimg.com/og/ed/f/original/2020/01/24/receita-de-camarao-empanado-receita-de-arroz-com-brocolis-molho-beurre-blanc.jpg'
+    },
+    {
+      title: 'Nosso delicioso camarão na panela',
+      description: 'Delicie-se com o sabor do mar em sua própria cozinha! Apresentamos o Camarão na Panela, a estrela do seu próximo jantar. Frescor, praticidade e um sabor irresistível, tudo em um só prato. Leve para casa essa experiência gourmet e transforme suas refeições em momentos inesquecíveis. Experimente agora!',
+      url: 'https://www.divinho.com.br/blog/wp-content/uploads/2021/08/Receitas-com-Camar%C3%A3o.jpg'
+    },
+  ]
+
+export const shrimpImage1 = data[0].url
+export const shrimpTitle1 = data[0].title
+export const shrimpDescription1 = data[0].description
+export const shrimpImage2 = data[1].url
+export const shrimpTitle2 = data[1].title
+export const shrimpDescription2 = data[1].description
+export const shrimpImage3 = data[2].url
+export const shrimpTitle3 = data[2].title
+export const shrimpDescription3 = data[2].description
+
+export const restaurantCopy1 = 'Na Casa do Camarão, cada prato é uma celebração do sabor do mar! Delicie-se com a frescura e a qualidade incomparáveis dos nossos frutos do mar. Das grelhas aos molhos irresistíveis, cada opção do nosso cardápio é cuidadosamente preparada para satisfazer os paladares mais exigentes. Em um ambiente acolhedor e charmoso, cada visita se torna uma experiência memorável. Venha se deliciar conosco e descubra por que somos o destino preferido dos amantes de frutos do mar. Reserve sua mesa agora e deixe-nos levá-lo em uma jornada gastronômica inesquecível na Casa do Camarão!'
+export const restaurantCopyImage1 = 'https://imagens-cdn.canalrural.com.br/2024/01/camarao-camaroes-defeso-mpa.jpeg'
+
+export const restaurantCopy2 = 'Na Casa do Camarão, não apenas os pratos são uma festa para os sentidos, mas também o ambiente em si é uma obra-prima. Situado em um cenário encantador, nosso restaurante combina o calor da hospitalidade com uma estética que evoca o charme do litoral. Desde o momento em que você pisa em nosso espaço, será cativado pela atmosfera acolhedora e pelo design cuidadosamente elaborado. Dos elegantes interiores à atmosfera relaxante do nosso espaço ao ar livre, cada detalhe foi pensado para criar uma experiência verdadeiramente memorável. Venha nos visitar e descubra a perfeita harmonia entre gastronomia de classe mundial e uma estrutura deslumbrante na Casa do Camarão. Estamos ansiosos para recebê-lo em nosso paraíso à beira-mar.'
+export const restaurantCopyImage2 = 'https://www.wellplated.com/wp-content/uploads/2023/06/Grilled-Shrimp-Best-Ever.jpg'
+
+export const actualYear = new Date().getFullYear()
