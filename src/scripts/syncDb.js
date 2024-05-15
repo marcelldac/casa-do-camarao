356cfa7a975fabf8398fc72a098a2d8782017041
marcelldac <dacmarcell@gmail.com>
2024-05-14 21:22:12 -0300

    feat: Add syncDb script to synchronize the database

---
+++ src/scripts/syncDb.js
@@ -0,0 +1,15 @@
+import sequelize from '../config/database.js';
+import FormSubmission from '../models/FormSubmission.js';
+
+const syncDb = async () => {
+  try {
+    await sequelize.sync({ force: process.env.NODE_ENV === 'development' ? true : false });
+    console.log('Database synchronized successfully.');
+  } catch (error) {
+    console.error('Error synchronizing the database:', error);
+  } finally {
+    await sequelize.close();
+  }
+};
+
+syncDb();
