7a77b71005b0e338afc296c22856f7bd2f5bafb3
marcelldac <dacmarcell@gmail.com>
2024-05-14 23:36:39 -0300

    refactor: Update multer configuration for file uploads

---
+++ src/config/multer.js
@@ -0,0 +1,31 @@
+import multer from 'multer';
+import path from 'node:path';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const fileTypes = /pdf|doc|docx/;
+  const mimeType = fileTypes.test(file.mimetype);
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+  if (mimeType && extname) {
+    return cb(null, true);
+  }
+  cb('Error: File upload only supports the following filetypes - ' + fileTypes);
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // 5MB
+  fileFilter: fileFilter
+});
+
+export default upload;
