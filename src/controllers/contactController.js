e8d0cd1b8fc109abddca248018d172d4c74f39fc
marcelldac <dacmarcell@gmail.com>
2024-05-14 23:37:03 -0300

    feat: Add resume upload functionality to contact form

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,18 +1,20 @@
 import { actualYear } from '../data/data.js';
 import FormSubmission from '../models/FormSubmission.js';
 
-export const renderContact = (req, res) => {
+export const renderContact = (_, res) => {
   res.render('contact', { actualYear });
 };
 
 export const submitForm = async (req, res) => {
   const { email, message } = req.body;
+  const resume = req.file ? req.file.filename : null;
+
   if (!email || !message) {
     return res.status(400).send("Dados incompletos, formulário não foi enviado. Tente novamente.");
   }
 
   try {
-    await FormSubmission.create({ email, message });
+    await FormSubmission.create({ email, message, resume });
     res.send('Formulário enviado com sucesso!');
   } catch (error) {
     console.error('Error saving form submission:', error);
@@ -20,7 +22,7 @@ export const submitForm = async (req, res) => {
   }
 };
 
-export const viewSubmissions = async (req, res) => {
+export const viewSubmissions = async (_, res) => {
   try {
     const submissions = await FormSubmission.findAll();
     res.render('submissions', { submissions, actualYear });
