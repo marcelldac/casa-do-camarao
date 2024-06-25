4e02758f2deb435f7e8402c28cbf9ba235456963
marcelldac <devmarcell@protonmail.com>
2024-06-25 01:44:32 -0300

    refactor: change from sqlite to mysql

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,18 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite'
+  dialect: "mysql",
+  host: "localhost",
+  username: "root",
+  password: "root",
+  database: "casaCamarao",
+  port: 3306,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 export default sequelize;
